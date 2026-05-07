2b4e7f11369f28ecb8598878180b19240c6deab5
Alexey <lavrik-aleksey-1@yandex.ru>
2026-05-07 22:36:25 +0300

    implement TaskGenerator for generating a set of tasks

---
+++ src/task_generator/task_generator.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <vector>
+#include <random>
+
+#include "task.h"
+
+namespace tps::task_generator {
+
+class TaskGenerator {
+public:
+    TaskGenerator(int min_seconds = 1, int max_seconds = 10)
+        : distribution_(min_seconds, max_seconds),
+          random_engine_(std::random_device{}()) {}
+
+    std::vector<task::Task> Generate(std::size_t task_count) {
+        std::vector<task::Task> tasks;
+        tasks.reserve(task_count);
+
+        for (std::size_t i = 0, ie = task_count; i != ie; ++i) {
+            task::Task task {
+                .name = "task_" + std::to_string(i + 1),
+                .payload = GetRandomInt(),
+                .delay = GetRandomInt()
+            };
+            tasks.push_back(task);
+        }
+
+        return tasks;
+    }
+
+private:
+    std::uniform_int_distribution<int> distribution_;
+    std::mt19937 random_engine_;
+
+    int GetRandomInt() {
+        return distribution_(random_engine_);
+    }
+};
+
+} // namespace tps::task_generator
