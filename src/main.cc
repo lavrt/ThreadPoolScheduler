a0cd4b5482387f92a245596dfa53c8098c88eb61
Alexey <lavrik-aleksey-1@yandex.ru>
2026-05-09 00:49:04 +0300

    update main: use logger

--- src/main.cc
+++ src/main.cc
@@ -2,15 +2,24 @@
 
 #include "cl_parser.h"
 #include "task_generator.h"
+#include "thread_pool.h"
+#include "logger.h"
 
 int main(int argc, const char* argv[]) {
     try {
         auto config = tps::cl_parser::ParseCl(argc, argv);
-        
-        auto tasks = tps::task_generator::TaskGenerator{}.Generate(config.task_count);
 
+        tps::logging::AsyncLogger logger;
+
+        auto tasks = tps::task_generator::TaskGenerator{}.Generate(config.task_count);
+        logger.Post(tps::logging::TasksGenerated{config.task_count});
         for (auto&& task : tasks) {
-            std::cout << task.name << " " << task.payload << " " << task.delay << "\n";
+            logger.Post(tps::logging::TaskInfo{task.name, task.payload, task.delay});
+        }
+
+        tps::thread_pool::ThreadPool pool(config.thread_count);
+        for (auto&& task : tasks) {
+            pool.Submit(task);
         }
 
     } catch (const std::exception& e) {
