db582fd6eecc8360a88a445b560130337688fa6a
Alexey <lavrik-aleksey-1@yandex.ru>
2026-05-09 14:50:37 +0300

    replace the standard library algorithm with a loop

--- src/main.cc
+++ src/main.cc
@@ -15,11 +15,11 @@ int main(int argc, const char* argv[]) {
         logging::AsyncLogger logger(std::cout);
 
         auto tasks = task_generator::TaskGenerator{}.Generate(cfg.task_count);
+        
         logger.Post(logging::TasksGenerated{cfg.task_count});
-
-        std::for_each(tasks.begin(), tasks.end(), [&logger](auto&& task) {
+        for (auto&& task : tasks) {
             logger.Post(logging::TaskInfo{task.name, task.payload, task.delay});
-        });
+        }
 
         thread_pool::ThreadPool pool(cfg.thread_count, logger);
         for (auto&& task : tasks) {
