4a432c735dd0f6688c6ab7cffcb13416142bd4cb
Alexey <lavrik-aleksey-1@yandex.ru>
2026-05-10 15:05:55 +0300

    implement constructor for Task; add automatic calculation ready_at; make Task callable

--- src/task/task.h
+++ src/task/task.h
@@ -2,14 +2,31 @@
 
 #include <string>
 #include <chrono>
+#include <functional>
 
 namespace tps::task {
 
 struct Task {
     std::string name;
-    int payload{};
-    int delay{};
+    int payload;
+    int delay;
+    std::function<void()> work;
     std::chrono::steady_clock::time_point ready_at;
+
+    Task(std::string task_name,
+         int task_payload,
+         int task_delay,
+         std::function<void()> task_work)
+        : name(std::move(task_name)),
+          payload(task_payload),
+          delay(task_delay),
+          work(std::move(task_work)),
+          ready_at(std::chrono::steady_clock::now() +
+                   std::chrono::seconds(task_delay)) {}
+
+    void operator()() {
+        work();
+    }
 };
 
 } // namespace tps::task
