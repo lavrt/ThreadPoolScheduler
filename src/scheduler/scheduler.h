5c9c679b6a474926bfeb590ce59b139f04b9445d
Alexey <lavrik-aleksey-1@yandex.ru>
2026-05-11 17:05:20 +0300

    remove unused std::move

--- src/scheduler/scheduler.h
+++ src/scheduler/scheduler.h
@@ -98,7 +98,7 @@ private:
             const auto ready_at = task_queue_.top().activation_time;
 
             if (ready_at <= Clock::now()) {
-                auto active_task = std::move(task_queue_.top());
+                auto active_task = task_queue_.top();
                 task_queue_.pop();
                 
                 lock.unlock();
